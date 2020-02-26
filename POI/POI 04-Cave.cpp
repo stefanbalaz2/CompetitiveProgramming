3b5f706bfc26e8218d2365cf914c88d0838b6a83
stefanbalaz2 <58517366+stefanbalaz2@users.noreply.github.com>
2020-02-26 14:33:38 +0100

    Update POI 04-Cave.cpp

--- POI/POI 04-Cave.cpp
+++ POI/POI 04-Cave.cpp
@@ -1,7 +1,39 @@
 /*
 
     idea: https://github.com/mostafa-saad/MyCompetitiveProgramming/blob/master/Olympiad/POI/official/2004/editorial/jas.pdf
-
+    
+    clarification: "choosing a node" means choosing a node which splits the graph into subtrees of its children
+    which narrows down our search
+    
+    the implemetation is the same as the editorial, but i didnt see a point guessing a random function like they did
+    so i tried to find an intuitive exlpanation for what is actually happening in the editorial
+    
+    look at it like this, we will root the tree in any node(its not important what is the root), and then we will do a dfs
+    on the tree, and solve the problem for every nodes subtree
+    
+    lets say that you are currently in node U trying to solve its subtree, and for some child of U lets inspect what 
+    we are keeping, for every solution J(number of moves needed to solve its subgraph, i will explain later what i mean by subgraph)
+    we are keeping the number of nodes which solve their subgraph in J moves(these numbers will be <=1 always for a node)
+    
+    now ill explain what i mean by "nodes subgraph"
+    imagine that when solving a subtree of U it can be solved in K moves, so we have a node that if we choose it as the first one
+    we can solve the pieces that remain in K-1, lets choose such a vertex that it is the highest in the subtree of U, meaning,
+    such vertex that choosing it first we can solve the remaining pieces in K-1 moves and that means that if we move our "starting node"
+    up the result for the subtree of U would increase to K+1 (which of course we dont want to)
+    lets call that node P,this makes the nodes below P irrelevant, and they will not be included in any more calculations(you will
+    see why soon)
+    for our node P, node P`s subgraph is the whole subtree U
+    lets observe the subgraph made as a (subtree of U without subtree of P), we will call it T, 
+    those are all nodes of subtree U that are above P
+    when "choosing" the P, the subtree of U splits into pieces, on of them being T
+    so we can observe that we have the same stuff for T that we had for our subtree of U, we will have a node that if "chosen"
+    first,we need Q moves to clear T, and as that node is the highest in T with such property(that it needs Q moves), the nodes in T
+    which are below it become irrelevant(the same stuff as with P happen), so then we are again left with 
+    (subgraph T without the subtree of T`s "starting node")
+    the process repeats until we are left with 1 node which is trivial
+    
+    
+    
 */
 #include<bits/stdc++.h>
 #define ff first
