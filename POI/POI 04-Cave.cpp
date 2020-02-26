3d7566d74976e68785e7c9174cb393d222260721
stefanbalaz2 <58517366+stefanbalaz2@users.noreply.github.com>
2020-02-26 15:24:20 +0100

    Update POI 04-Cave.cpp

--- POI/POI 04-Cave.cpp
+++ POI/POI 04-Cave.cpp
@@ -25,14 +25,38 @@
     for our node P, node P`s subgraph is the whole subtree U
     lets observe the subgraph made as a (subtree of U without subtree of P), we will call it T, 
     those are all nodes of subtree U that are above P
-    when "choosing" the P, the subtree of U splits into pieces, on of them being T
+    when "choosing" the P, the subtree of U splits into pieces, one of them being T
     so we can observe that we have the same stuff for T that we had for our subtree of U, we will have a node that if "chosen"
     first,we need Q moves to clear T, and as that node is the highest in T with such property(that it needs Q moves), the nodes in T
     which are below it become irrelevant(the same stuff as with P happen), so then we are again left with 
     (subgraph T without the subtree of T`s "starting node")
     the process repeats until we are left with 1 node which is trivial
     
+    an illustration to make it easier to understand: https://github.com/stefanbalaz2/CompetitiveProgramming/blob/master/POI/Untitled.png
     
+    so how are we gonna do it?
+    in every node we are keeping how much nodes in its subtrees are there that solve their subgraph in J moves
+    when we are solving for U, we add up all the values of U`s children, and find the largest J such that its value is >=2
+    why?
+    because, if we have some value>=2 on position J, that means that we have more than 1 children that have a subgraph which
+    solution is J which is still pending for merging( ill explain that soon)
+    so those subgraphs are merged by our U and we cant solve them all together in J moves, but we can solve them in J+1 moves
+    by taking U first and then just doing those pieces the way that they were supposed to be done in J moves
+    so we get a vertex with value J+1, making all those subgraphs that we merged irrelevant because they must be solved in
+    that way that we mentioned earlier(so we can set the values of all below J+1 to 0)
+    but thats not all, observe that if we had a node with value of J+1, we cant solve it and this new merged subgraph in 
+    J+1 all together, but we can solve them in J+2, so we get a subgraph solvable in J+2, but what if we again have a node
+    with J+2, and so on... so we are merging all thhose into a bigger and bigger component till we dont have anything to merge
+    anymore, then we get to some value of E and we put on position E value 1, and everything <E to 0 because they are irrelevant because
+    we cant get anything better with considering those nodes
+    
+    so what is our solution?
+    for every node we calculated number of moves to solve its subgraph(we said earlier what do we mean by a subgraph)
+    so there must exist a node with the largest value which actually solves our whole graph and thats our answer
+    
+    another thing, from our explanation it is obvious that the best way to solve the graph if we had to print out moves is to
+    for a current subgraph take the largest valued node,then split and ur current subgraph becomes the one which includes the 
+    node u got back from a query
     
 */
 #include<bits/stdc++.h>
