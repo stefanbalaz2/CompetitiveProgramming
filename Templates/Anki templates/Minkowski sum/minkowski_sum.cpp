f3b78a426add679c2b52a310bbdd25c7b3c55e39
stefanbalaz2 <58517366+stefanbalaz2@users.noreply.github.com>
2022-09-03 13:53:15 +0200

    Update minkowski_sum.cpp

--- Templates/Anki templates/Minkowski sum/minkowski_sum.cpp
+++ Templates/Anki templates/Minkowski sum/minkowski_sum.cpp
@@ -16,57 +16,46 @@ struct pt{
 
     ll x,y;
 
-    pt(ll x,ll y){
-        this->x=x;
-        this->y=y;
+    pt operator +(pt &a){
+        return pt(x+a.x,y+a.y);
     }
-
-    pt operator + (const pt &a)const{
-        return pt(a.x+this->x,a.y+this->y);
+    pt operator -(pt &a){
+        return pt(x-a.x,y-a.y);
     }
-    pt operator - (const pt &a)const{
-        return pt(-a.x+this->x,-a.y+this->y);
-    }
-
-    ll cross(const pt &a){
-        return this->x*a.y-this->y*a.x;
+    ll cross(pt &a){
+        return x*a.y-y*a.x;
     }
 
 };
 
-int get_min_pos(vector<pt>&a){
+int get_pt(vector<pt>&a){
 
     int ret=0;
-    for(int i=1;i<a.size();i++){
-        if( a[i].y<a[ret].y || ( a[i].y==a[ret].y && a[i].x<a[ret].x ) )
-            ret=i;
-    }
-
+    for(int i=1;i<a.size();i++)
+        if( a[i].y<a[ret].y || (a[i].y==a[ret].y && a[i].x<a[ret].x) )ret=i;
     return ret;
 }
 
-vector<pt>minkowski_sum(vector<pt>a,vector<pt>b){
+vector<pt> minkowski(vector<pt>a,vector<pt>b){
 
-    int posa=get_min_pos(a);
-    int posb=get_min_pos(b);
+    int posa=0,posb=0;
+    posa=get_pt(a);
+    posb=get_pt(b);
     rotate(a.begin(),a.begin()+posa,a.end());
-    rotate(b.begin(),b.begin()+posa,b.end());
+    rotate(b.begin(),b.begin()+posb,b.end());
 
     a.pb(a[0]);
-    b.pb(b[0]);
     a.pb(a[1]);
+    b.pb(b[0]);
     b.pb(b[1]);
 
-    int pta=0;
-    int ptb=0;
+    int pa=0;
+    int pb=0;
+    while( !(pa==a.size()-2 && pb==b.size()-2) ){
 
-    while( !(pta==a.size()-2 && ptb=b.size()-2) ){
-
-        ret.pb(a[pta]+b[pta]);
-
-        if( (a[pta+1]-a[pta]).cross(b[ptb+1]-b[ptb])>=0 )pta++;
-        if( (b[ptb+1]-a[ptb]).cross(a[pta+1]-b[pta])>=0 )ptb++;
-
+        ret.pb(a[pa]+b[pb]);
+        if( (a[pa+1]-a[pa]).cross(b[pb+1]-b[pb])>=0)pa++;
+        if( (b[pb+1]-b[pb]).cross(a[pa+1]-a[pa])>=0)pb++;
     }
 
     return ret;
