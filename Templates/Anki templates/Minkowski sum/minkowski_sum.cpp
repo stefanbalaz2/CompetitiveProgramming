a72a245b4972d274df106272338772b709e0dae0
stefanbalaz2 <58517366+stefanbalaz2@users.noreply.github.com>
2022-12-13 14:25:32 +0100

    Update minkowski_sum.cpp

--- Templates/Anki templates/Minkowski sum/minkowski_sum.cpp
+++ Templates/Anki templates/Minkowski sum/minkowski_sum.cpp
@@ -1,71 +1,64 @@
-/**
-
-
-
-*/
 #include <bits/stdc++.h>
 using namespace std;
 #define ff first
 #define ss second
 #define pb push_back
 #define ll long long
-#define ull unsigned long long
 typedef pair<int,int> pii;
 
 struct pt{
-
-    ll x,y;
-
-    pt operator +(pt &a){
-        return pt(x+a.x,y+a.y);
+    int x,y;
+    pt(int x,int y){
+        this->x=x;
+        this->y=y;
+    }
+    ll cross(pt a){
+        return (ll)x*a.y-(ll)y*a.x;
     }
     pt operator -(pt &a){
         return pt(x-a.x,y-a.y);
     }
-    ll cross(pt &a){
-        return x*a.y-y*a.x;
+    pt operator +(pt &a){
+        return pt(x+a.x,y+a.y);
     }
-
 };
+int get_mp(vector<pt>&a){
 
-int get_pt(vector<pt>&a){
-
-    int ret=0;
-    for(int i=1;i<a.size();i++)
-        if( a[i].y<a[ret].y || (a[i].y==a[ret].y && a[i].x<a[ret].x) )ret=i;
-    return ret;
+    int id=0;
+    for(int i=1;i<a.size();i++){
+        if(a[i].x<a[id].x)id=i;
+        else if(a[i].x==a[id].x && a[i].y<a[id].y)id=i;
+    }
+    return id;
 }
-
 vector<pt> minkowski(vector<pt>a,vector<pt>b){
 
-    int posa=0,posb=0;
-    posa=get_pt(a);
-    posb=get_pt(b);
-    rotate(a.begin(),a.begin()+posa,a.end());
-    rotate(b.begin(),b.begin()+posb,b.end());
+    int id=get_mp(a);
+    rotate(a.begin(),a.begin()+id,a.end());
+    id=get_mp(b);
+    rotate(b.begin(),b.begin()+id,b.end());
 
     a.pb(a[0]);
     a.pb(a[1]);
     b.pb(b[0]);
     b.pb(b[1]);
 
-    int pa=0;
-    int pb=0;
-    while( !(pa==a.size()-2 && pb==b.size()-2) ){
-
-        ret.pb(a[pa]+b[pb]);
-        int p2=(b[pb+1]-b[pb]).cross(a[pa+1]-a[pa]);
-        if( (a[pa+1]-a[pa]).cross(b[pb+1]-b[pb])>=0)pa++;
-        if( p2>=0)pb++;
+    int pt1=0;
+    int pt2=0;
+    vector<pt>ret;
+    while( !( pt1==a.size()-2 && pt2==b.size()-2 ) ){
+        ret.pb(a[pt1]+b[pt2]);
+        ll cs=(a[pt1+1]-a[pt1]).cross(b[pt2+1]-b[pt2]);
+        if(cs>=0)pt1++;
+        if(cs<=0)pt2++;
     }
-
     return ret;
 }
 
 int main() {
 
     ///freopen("test.txt","r",stdin);
-    ///freopen("moj.txt","w",stdout);
+
 
 
 
