d6ca13117de0410c950bf3c030ace2c6eb1d9747
stefanbalaz2 <58517366+stefanbalaz2@users.noreply.github.com>
2023-08-10 22:21:09 +0200

    Update custom_bitwise_convolutions.cpp

--- Templates/Anki templates/FFT variations/custom_bitwise_convolutions.cpp
+++ Templates/Anki templates/FFT variations/custom_bitwise_convolutions.cpp
@@ -9,161 +9,155 @@ using namespace std;
 #define ss second
 #define pb push_back
 #define ll long long
-#define ull unsigned long long
 typedef pair<int,int> pii;
 
+const int maxn=(1<<18);
+
+int n;
+
 struct mat{
-
     int a[2][2];
-
-    int det(){
-        return a[0][0]*a[1][1]-a[1][0]*a[0][1];
-    }
-
-    mat inv(){
-
-        mat ret;
-        int d=det();
-        if(d==0)return ret;
-        ret.a[0][0]=(a[1][1]*2)/d;
-        ret.a[1][1]=(2*a[0][0])/d;
-        ret.a[0][1]=-(2*a[0][1])/d;
-        ret.a[1][0]=-(2*a[1][0])/d;
-
-        return ret;
-    }
-
     void ispis(){
-
-        printf("%d %d \n %d %d  mat\n",a[0][0],a[0][1],a[1][0],a[1][1]);
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                printf("%d ",a[i][j]);
+            }
+            printf("\n");
+        }
+        printf("gotova MAT\n");
     }
-
 };
+struct trans{
+    mat a[3];
+    void ispis(){
+        for(int i=0;i<3;i++){
+            a[i].ispis();
+        }
+        printf("GOTOV TRANS\n");
+    }
+};
+int det(mat a){
+    return a.a[0][0]*a.a[1][1]-a.a[0][1]*a.a[1][0];
+}
+mat inverse(mat a){
 
-bool check(mat f,mat a,mat b,mat c){
+    int d=det(a);
 
-    if(c.det()==0)return 0;
+    swap(a.a[0][0],a.a[1][1]);
+    a.a[1][0]=-a.a[1][0];
+    a.a[0][1]=-a.a[0][1];
+
+    a.a[0][0]*=2;
+    a.a[0][1]*=2;
+    a.a[1][0]*=2;
+    a.a[1][1]*=2;
+
+    a.a[0][0]/=d;
+    a.a[0][1]/=d;
+    a.a[1][0]/=d;
+    a.a[1][1]/=d;
+
+    return a;
+    /// returns inverse multiplied with 2
+}
+bool check(trans x,mat y){
 
     for(int i=0;i<2;i++)
-    for(int j=0;j<2;j++)
-        for(int k=0;k<2;k++)
-            if(a.a[i][k]*b.a[j][k]!=c.a[f.a[i][j]][k])return false;
+        for(int j=0;j<2;j++)
+            for(int k=0;k<2;k++)
+                if(x.a[0].a[i][k]*x.a[1].a[j][k]!=x.a[2].a[y.a[i][j]][k])return false;
+
+    if(det(x.a[2])==0)return false;
 
     return true;
 }
-
-pair<mat,pair<mat,mat> > get_f(mat f){
-
-
-    mat a,b,c;
+trans get_trans(mat a){
 
     for(int i=0;i<531441;i++){
 
-        int cc=i;
-        for(int j=0;j<4;j++){
-            a.a[j/2][j%2]=cc%3-1;
-            cc/=3;
-        }
-        for(int j=0;j<4;j++){
-            b.a[j/2][j%2]=cc%3-1;
-            cc/=3;
-        }
-        for(int j=0;j<4;j++){
-            c.a[j/2][j%2]=cc%3-1;
-            cc/=3;
-        }
+        int mask=i;
 
+        trans pom;
+        for(int j=0;j<3;j++)
+            for(int k=0;k<2;k++)
+                for(int k2=0;k2<2;k2++){
+                    pom.a[j].a[k][k2]=mask%3-1;
+                    mask/=3;
+                }
 
-        if(check(f,a,b,c))return {c.inv(),{a,b}};
-
+        if(check(pom,a))return pom;
     }
 
 }
 
-void fft(vector<ll>&a,vector<mat>ts,bool invert){
+void fft(vector<ll>&a,bool invert,vector<trans>trs,int c){
     int n=a.size();
-    int c=-1;
+    int cnt=-1;
     for(int len=2;len<=n;len<<=1){
-        c++;
-        int hlen=len/2;
+
+        cnt++;
+        mat ct=trs[cnt].a[c];
+        if(invert)ct=inverse(ct);
 
         for(int i=0;i<n;i+=len){
 
-            for(int j=0;j<hlen;j++){
+            for(int j=0;j<len/2;j++){
 
-                ll pom1=a[i+j]*ts[c].a[0][0]+a[i+j+hlen]*ts[c].a[1][0];
-                ll pom2=a[i+j]*ts[c].a[0][1]+a[i+j+hlen]*ts[c].a[1][1];
+                ll pom1=a[i+j];
+                ll pom2=a[i+j+len/2];
 
-                a[i+j]=pom1;
-                a[i+j+hlen]=pom2;
+                a[i+j]=pom1*ct.a[0][0]+pom2*ct.a[1][0];
+                a[i+j+len/2]=pom1*ct.a[0][1]+pom2*ct.a[1][1];
 
                 if(invert){
                     a[i+j]/=2;
-                    a[i+j+hlen]/=2;
+                    a[i+j+len/2]/=2;
                 }
-
             }
 
         }
 
     }
 
+
 }
-
-vector<ll>pmul(vector<ll>a,vector<ll>b,vector<mat>A,vector<mat>B,vector<mat>C){
-
-    fft(a,A,0);
-    fft(b,B,0);
+vector<ll> pol_mul(vector<ll>a,vector<ll>b,vector<trans>trs){
+    fft(a,0,trs,0);
+    fft(b,0,trs,1);
     for(int i=0;i<a.size();i++)a[i]*=b[i];
-    fft(a,C,1);
-    ///for(int i=0;i<a.size();i++)a[i]/=a.size();
-
+    fft(a,1,trs,2);
     return a;
 }
 
 int main() {
 
+
     ///freopen("test.txt","r",stdin);
-    ///freopen("moj.txt","w",stdout);
 
-    int n;
     scanf("%d",&n);
-    vector<mat>A,B,C;
-    for(int i=0;i<n;i++){
 
+    vector<trans>pom;
+    for(int i=0;i<n;i++){
         string s;
         cin>>s;
-
-        mat pom;
-        for(int i=0;i<4;i++)
-            pom.a[i/2][i%2]=s[i]-'0';
-
-        pair<mat,pair<mat,mat> >pp=get_f(pom);
-
-        /*printf("AAA\n");
-
-        pp.ff.ispis();
-        pp.ss.ff.ispis();
-        pp.ss.ss.ispis();*/
-
-        C.pb(pp.ff);
-        A.pb(pp.ss.ff);
-        B.pb(pp.ss.ss);
+        mat p;
+        p.a[0][0]=s[0]-'0';
+        p.a[0][1]=s[1]-'0';
+        p.a[1][0]=s[2]-'0';
+        p.a[1][1]=s[3]-'0';
+        pom.pb(get_trans(p));
     }
 
-
-    vector<ll>a((1<<n)),b(1<<n);
-
+    vector<ll>a((1<<n)),b((1<<n));
     for(int i=0;i<(1<<n);i++)
         scanf("%lld",&a[i]);
-
     for(int i=0;i<(1<<n);i++)
         scanf("%lld",&b[i]);
 
-    a=pmul(a,b,A,B,C);
+    a=pol_mul(a,b,pom);
 
-    for(int i=0;i<(1<<n);i++)printf("%lld ",a[i]);
-
+    for(int i=0;i<a.size();i++)printf("%lld ",a[i]);
 
     return 0;
 }
+
