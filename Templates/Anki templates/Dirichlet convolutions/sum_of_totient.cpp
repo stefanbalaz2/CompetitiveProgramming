67c8063e49c149c84f954cf05ee91682823b2334
stefanbalaz2 <58517366+stefanbalaz2@users.noreply.github.com>
2024-01-12 02:50:17 +0100

    Create sum_of_totient.cpp

---
+++ Templates/Anki templates/Dirichlet convolutions/sum_of_totient.cpp
@@ -0,0 +1,150 @@
+#include<bits/stdc++.h>
+#define ff first
+#define ss second
+#define pb push_back
+typedef long long ll;
+using namespace std;
+using u128=__uint128_t;
+typedef pair<int,int> pii;
+
+const int mod=998244353;
+inline int add(int x,int y){int ret=x+y;if(ret>=mod)ret-=mod;return ret;}
+inline int sub(int x,int y){int ret=x-y;if(ret<0)ret+=mod;return ret;}
+inline int mul(int x,int y){return ((ll)x*y)%mod;}
+inline int step(int base,int pw){int ret=1;while(pw){if(pw&1)ret=mul(ret,base);base=mul(base,base);pw>>=1;}return ret;}
+inline int invv(int x){return step(x,mod-2);}
+
+const int maxn=1e5+10;
+
+ll n;
+
+struct rieman{
+    int f(ll n){
+        return 1;
+    }
+    int F(ll n){
+        return n%mod;
+    }
+};
+
+struct mertens{
+
+    vector<int>F,f,a;
+    ll n,y,k;
+
+    mertens(ll n){
+
+        this->n=n;
+        y=pow(n,0.67);
+        k=n/y;
+        F.resize(y+1);
+        f.resize(y+1);
+
+        do_sieve();
+        precalc_a();
+    }
+
+    int get_F(ll k){
+        if(n/k<=y)return F[n/k];
+        return a[a.size()-k];
+    }
+
+    void do_sieve(){
+
+
+        f[1]=1;
+        vector<int>primes;
+        vector<bool>composite(y+1);
+        for(int i=2;i<=y;i++){
+            if(!composite[i]){
+                primes.pb(i);
+                f[i]=mod-1;
+            }
+
+            for(int j=0;j<primes.size() && i*primes[j]<=y;j++){
+
+                composite[i*primes[j]]=true;
+
+                if(i%primes[j]==0){
+                    f[i*primes[j]]=0;
+                    break;
+                }
+                else{
+                    f[i*primes[j]]=mul(f[i],mod-1);
+                }
+
+            }
+
+        }
+        for(int i=1;i<=y;i++)F[i]=add(F[i-1],f[i]);
+
+    }
+
+    int calculate_big_F(ll k){/// calculate F(n/k)
+
+        rieman R;
+
+        ll cn=n/k;
+        int b=sqrt(cn);
+
+        int ret=1;
+        for(int i=2;i<=b;i++)
+            ret=sub(ret, mul(R.f(i),get_F(k*i)) );
+
+        for(int i=1;i<=b;i++)
+            ret=sub(ret, mul(f[i],R.F(n/(k*i))) );
+
+        ll ts=n/k;
+        ret=add(ret,mul(R.F(b),get_F( (ts/b)*k )));
+
+        return ret;
+    }
+
+    void precalc_a(){
+
+        int pt=0;
+        a.resize(k);
+        for(int j=k;j>=1;j--){
+            a[pt++]=calculate_big_F(j);
+        }
+    }
+
+};
+
+
+struct identity{
+
+    int inv2=invv(2);
+
+    int f(ll n){
+        return n%mod;
+    }
+    int F(ll n){
+        return mul(mul(add(n%mod,1),n%mod),inv2);
+    }
+
+};
+
+
+int main(){
+
+    ///freopen("test.txt","r",stdin);
+
+    scanf("%lld",&n);
+    mertens m(n);
+
+    int ret=0;
+
+    identity id;
+
+    ll b=sqrt(n);
+    for(int i=1;i<=b;i++){
+        ret=add(ret,mul(id.f(i),m.get_F(i) ));
+        ret=add(ret,mul(id.F(n/i),m.f[i] ));
+    }
+    ret=sub(ret,mul( id.F(b),m.get_F(n/b) ));
+
+    printf("%d\n",ret);
+
+    return 0;
+}
