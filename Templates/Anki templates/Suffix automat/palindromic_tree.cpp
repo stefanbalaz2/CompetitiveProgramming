419fccf4e145152411ac997f9f4931f5868ec390
stefanbalaz2 <58517366+stefanbalaz2@users.noreply.github.com>
2024-09-26 15:40:34 +0200

    Update palindromic_tree.cpp

--- Templates/Anki templates/Suffix automat/palindromic_tree.cpp
+++ Templates/Anki templates/Suffix automat/palindromic_tree.cpp
@@ -1,16 +1,15 @@
-#include<cstdio>
-#include<algorithm>
-#include<cstring>
-#include<string>
-#include<iostream>
-#include<assert.h>
+/**
+    https://acm.timus.ru/problem.aspx?space=1&num=1960
+
+*/
+#include<bits/stdc++.h>
 #define ff first
 #define ss second
 #define pb push_back
-typedef long long ll;
 using namespace std;
-using u128=__uint128_t;
+typedef long long ll;
 typedef pair<int,int> pii;
+
 
 const int mod=998244353;
 inline int add(int x,int y){int ret=x+y;if(ret>=mod)ret-=mod;return ret;}
@@ -19,71 +18,57 @@ inline int mul(int x,int y){return ((ll)x*y)%mod;}
 inline int step(int base,int pw){int ret=1;while(pw){if(pw&1)ret=mul(ret,base);base=mul(base,base);pw>>=1;}return ret;}
 inline int invv(int x){return step(x,mod-2);}
 
-const int maxn=4e5+10;
+const int maxn=1e5+10;
 const int maxalp=26;
-int link[maxn],last,trans[maxn][maxalp],len[maxn],cnt;
 string s;
+int cnt,link[maxn],len[maxn],trans[maxn][maxalp],last;
+void prep(){
 
-void init(){
-
-    memset(len,-1,sizeof(len));
-    memset(trans,-1,sizeof(trans));
     last=1;
     cnt=2;
     link[0]=0;
     link[1]=0;
     len[0]=-1;
     len[1]=0;
+    memset(trans,-1,sizeof(trans));
 
 }
-int append(int pos){
+int append(int pos,int c){
 
     int curr=last;
-    while(true){
-        if(pos-1-len[curr]>=0 && s[pos]==s[pos-1-len[curr]])break;
-        curr=link[curr];
-    }
-
-    int c=s[pos]-'a';
+    while(pos-len[curr]-1<0 || s[pos-len[curr]-1]!=s[pos]){curr=link[curr];}
 
     if(trans[curr][c]!=-1){
         last=trans[curr][c];
         return 0;
     }
 
-    int q=cnt++;
-    trans[curr][c]=q;
-    last=q;
-    len[q]=len[curr]+2;
+    last=cnt;
+    len[last]=len[curr]+2;
+    trans[curr][c]=cnt++;
 
-    if(curr==0){
-        link[q]=1;
-        return 1;
-    }
     curr=link[curr];
-    while(true){
-        if(pos-1-len[curr]>=0 && s[pos]==s[pos-1-len[curr]])break;
-        curr=link[curr];
-    }
-
-    link[q]=trans[curr][c];
+    while(pos-len[curr]-1<0 || s[pos-len[curr]-1]!=s[pos]){curr=link[curr];}
+    link[last]=trans[curr][c];
+    if(trans[curr][c]==last)link[last]=1;
 
     return 1;
-
 }
 
 int main(){
 
     ///freopen("test.txt","r",stdin);
-    //freopen("moj.txt","w",stdout);
 
-    init();
     cin>>s;
+
+    prep();
+
     int rez=0;
     for(int i=0;i<s.size();i++){
-        rez+=append(i);
+        rez+=append(i,s[i]-'a');
         printf("%d ",rez);
     }
+    printf("\n");
 
     return 0;
 }
